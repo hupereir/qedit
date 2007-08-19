aff5839a302f8f8482abb6092d79145d246c9aa9
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2007-08-19 21:30:04 +0000

    *** empty log message ***

--- src/AutoSave.h
+++ src/AutoSave.h
@@ -66,7 +66,7 @@ class AutoSave: public QObject, public Counter
   /* \param display if set to non 0, only threads that match the display are saved */
   void saveFiles( const TextDisplay* display = 0 );
   
-  protected slots:
+  private slots:
 
   //! update configuration 
   /* update interval between threads */
