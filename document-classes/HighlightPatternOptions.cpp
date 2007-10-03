135084e1d1b351b239d2bbcfe8fa260824658f55
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2007-10-03 11:17:54 +0000

    Highlight pattern options for Highlight Pattern dialog.

---
+++ document-classes/HighlightPatternOptions.cpp
@@ -0,0 +1,77 @@
+// $Id$
+
+/******************************************************************************
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple
+* Place, Suite 330, Boston, MA  02111-1307 USA
+*
+*
+*******************************************************************************/
+
+/*!
+   \file HighlightPatternOptions.cc
+   \brief font formatting options
+   \author Hugo Pereira
+   \version $Revision$
+   \date $Date$
+*/
+
+#include <QHBoxLayout>
+
+#include "Debug.h"
+#include "HighlightPatternOptions.h"
+#include "HighlightPattern.h"
+
+using namespace std;
+
+//_____________________________________________
+HighlightPatternOptions::HighlightPatternOptions( QWidget* parent ):
+  QWidget( parent )
+{
+  Debug::Throw( "HighlightPatternOptions::HighlightPatternOptions.\n" );
+  setLayout( new QHBoxLayout() );
+  layout()->setMargin(2);
+  layout()->setSpacing(2);
+  layout()->addWidget( span_ = new QCheckBox( "&Span", this ) );
+  layout()->addWidget( no_indent_ = new QCheckBox( "&No indentation", this ) );
+  layout()->addWidget( case_sensitive_ = new QCheckBox( "&Case sensitive", this ) );
+
+  span_->setToolTip( "Highlight pattern spans over several paragraphs" );
+  no_indent_->setToolTip( 
+    "Paragraphs matching the pattern are skipped from automatic indentation.\n"
+    "This is typically the case for comments." );
+  case_sensitive_->setToolTip( "Highlight pattern matching is case sensitive" );
+
+}
+
+//__________________________________________________
+void HighlightPatternOptions::setOptions( const unsigned int& options )
+{
+  Debug::Throw( "HighlightPatternOptions::setOptions.\n" );
+  span_->setChecked( options & HighlightPattern::SPAN );
+  no_indent_->setChecked( options & HighlightPattern::NO_INDENT );
+  case_sensitive_->setChecked( !(options & HighlightPattern::CASE_INSENSITIVE) );
+}
+
+//__________________________________________________
+unsigned int HighlightPatternOptions::options( void ) const
+{
+  unsigned int out = HighlightPattern::NONE;
+  if( span_->isChecked() ) out |= HighlightPattern::SPAN;
+  if( no_indent_->isChecked() ) out |= HighlightPattern::NO_INDENT;
+  if( !case_sensitive_->isChecked() ) out |= HighlightPattern::CASE_INSENSITIVE;
+  return out;
+}
---
+++ document-classes/HighlightPatternOptions.h
@@ -0,0 +1,65 @@
+#ifndef HighlightPatternOptions_h
+#define HighlightPatternOptions_h
+
+// $Id$
+
+/******************************************************************************
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple
+* Place, Suite 330, Boston, MA  02111-1307 USA
+*
+*
+*******************************************************************************/
+
+
+/*!
+   \file HighlightPatternOptions.h
+   \brief font formatting
+   \author Hugo Pereira
+   \version $Revision$
+   \date $Date$
+*/
+
+#include <QCheckBox>
+
+//! font formating
+class HighlightPatternOptions: public QWidget
+{
+
+  public:
+
+  //! constructor
+  HighlightPatternOptions( QWidget* parent );
+
+  //! set checkboxes from from
+  void setOptions( const unsigned int& options );
+
+  //! get format
+  unsigned int options( void ) const;
+
+  private:
+
+  //! bold 
+  QCheckBox* span_;
+  
+  //! italic
+  QCheckBox* no_indent_;
+  
+  //! underline
+  QCheckBox* case_sensitive_;
+    
+};
+#endif
