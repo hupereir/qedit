793c2a0f19434e1d71a88cf4cd111edce4705eca
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2007-12-29 11:13:31 +0000

    *** empty log message ***

--- src/FileSelectionDialog.cpp
+++ src/FileSelectionDialog.cpp
@@ -4,16 +4,16 @@
 * Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
 *
 * This is free software; you can redistribute it and/or modify it under the
-* terms of the GNU General Public License as published by the Free Software
-* Foundation; either version 2 of the License, or (at your option) any later
+* terms of the GNU General Public license as published by the Free Software
+* Foundation; either version 2 of the license, or (at your option) any later
 * version.
 *
 * This software is distributed in the hope that it will be useful, but WITHOUT
 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public license
 * for more details.
 *
-* You should have received a copy of the GNU General Public License along with
+* You should have received a copy of the GNU General Public license along with
 * software; if not, write to the Free Software Foundation, Inc., 59 Temple
 * Place, Suite 330, Boston, MA  02111-1307 USA
 *
@@ -35,10 +35,11 @@
 
 #include "Debug.h"
 #include "EditFrame.h"
-#include "MainFrame.h"
-#include "TextDisplay.h"
 #include "FileSelectionDialog.h"
+#include "MainFrame.h"
 #include "QtUtil.h"
+#include "TextDisplay.h"
+#include "TreeView.h"
 
 using namespace std;
 
@@ -56,11 +57,8 @@ FileSelectionDialog::FileSelectionDialog( QWidget* parent, const TextSelection& 
   setLayout( layout );
 
   // custom list view
-  list_ = new TreeWidget( this );
-  layout->addWidget( list_ );
-  list_->setColumnCount(2);
-  list_->setColumnName( FILE, "file name" );
-  list_->setColumnName( CLASS, "class" );
+  list_ = new TreeView( this );
+  list_->setModel( &model_ );
   list_->setSelectionMode( QAbstractItemView::MultiSelection );
 
   // store set of found files to avoid duplication
@@ -84,17 +82,14 @@ FileSelectionDialog::FileSelectionDialog( QWidget* parent, const TextSelection& 
 
       // retrieve document class
       const string& class_name( (*iter)->className() );
-
-      TreeWidget::Item* item( new TreeWidget::Item() );
-      list_->addTopLevelItem( item );
-      item->setText( FILE, file.expand().c_str() );
-      item->setText( CLASS, class_name.c_str() );
+      model_.add( std::make_pair( file.expand(), class_name ) );
 
     }
   }
 
-  list_->resizeColumnToContents( FILE );
+  list_->resizeColumnToContents( Model::FILE );
   list_->selectAll();
+  layout->addWidget( list_ );
 
   // button layout
   QHBoxLayout *button_layout( new QHBoxLayout() );
@@ -102,14 +97,13 @@ FileSelectionDialog::FileSelectionDialog( QWidget* parent, const TextSelection& 
   button_layout->setMargin(0);
   layout->addLayout( button_layout );
 
-  QPushButton* button;
-
   // replace
   button_layout->addWidget( button = new QPushButton( "&Replace", this ) );
   button->setToolTip( "Replace in all selected files" );
   connect( button, SIGNAL( clicked() ), this, SLOT( _replace() ) );
 
   // select all
+  QPushButton* button;  
   button_layout->addWidget( button = new QPushButton( "&Select All", this ) );
   button->setToolTip( "Select all files in list" );
   connect( button, SIGNAL( clicked() ), list_, SLOT( selectAll() ) );
@@ -134,12 +128,70 @@ void FileSelectionDialog::_replace( void )
   Debug::Throw( "FileSelectionDialog::_replace.\n" );
 
   // retrieve selection from the list
-  QList< QTreeWidgetItem* > items( list_->QTreeWidget::selectedItems() );
+  Model::List selection( model_.get( list_->selectionModel()->selectedRows() ) );
   list<File> files;
-  for( QList< QTreeWidgetItem* >::iterator iter = items.begin(); iter != items.end(); iter++ )
-  { files.push_back( File( qPrintable( (*iter)->text( FILE ) ) ) ); }
+  for( Model::List::iterator iter = selection.begin(); iter != selection.end(); iter++ )
+  { files.push_back( iter->first ); }
 
   emit fileSelected( files, selection_ );
   done( QDialog::Accepted );
+
+}
+
+//_______________________________________________
+const char* FileSelectionDialog::Model::column_titles_[ FileSelectionDialog::Model::n_columns ] =
+{ 
+  "file name",
+  "path",
+  "class"
+};
+
+//__________________________________________________________________
+QVariant FileSelectionDialog::Model::data( const QModelIndex& index, int role ) const
+{
+  Debug::Throw( "FileSelectionDialog::Model::data.\n" );
+  
+  // check index, role and column
+  if( !index.isValid() ) return QVariant();
+  
+  // retrieve associated file info
+  const FilePair& file( get(index) );
+  
+  // return text associated to file and column
+  if( role == Qt::DisplayRole ) {
+    
+    switch( index.column() )
+    {
+      case FILE:
+      return QString( file.first.localName().c_str() );
+
+      case PATH:
+      return QString( file.first.path().c_str() );
+      
+      case CLASS:
+      return QString( file.second.c_str() );
+      
+      default:
+      return QVariant();
+    }
+  }
+ 
+  return QVariant();
+  
+}
+
+//__________________________________________________________________
+QVariant FileSelectionDialog::Model::headerData(int section, Qt::Orientation orientation, int role) const
+{
+
+  if( 
+    orientation == Qt::Horizontal && 
+    role == Qt::DisplayRole && 
+    section >= 0 && 
+    section < n_columns )
+  { return QString( column_titles_[section] ); }
+  
+  // return empty
+  return QVariant(); 
 
 }
