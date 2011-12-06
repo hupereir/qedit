4a498023d6dfcfe717a159b4bd8a3ef5dc0150d3
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2011-12-06 15:38:49 +0000

    enable compilation of shared libraries

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-(cd $srcdir; aclocal -I /usr/share;\
+(cd $srcdir; aclocal -I /usr/share;
 libtoolize --force;
 automake --add-missing;
 autoconf)
