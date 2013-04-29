9be336012aa01aeae2c0033d95536c72afa4979f
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2013-04-29 18:27:13 +0000

    fixed license

--- src/NavigationFrame.cpp
+++ src/NavigationFrame.cpp
@@ -4,20 +4,18 @@
 *
 * This is free software; you can redistribute it and/or modify it under the
 * terms of the GNU General Public License as published by the Free Software
-* Foundation; either version 2 of the License, or (at your option) any later-
+* Foundation; either version 2 of the License, or (at your option) any later
 * version.
 *
 * This software is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+* Any WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 * for more details.
 *
 * You should have received a copy of the GNU General Public License along with
-* software; if not, write to the Free Software Foundation, Inc., 59 Temple
-* Place, Suite 330, Boston, MA  02111-1307 USA
+* this program.  If not, see <http://www.gnu.org/licenses/>.
 *
-*
-****************************************************************************/
+*******************************************************************************/
 
 #include "NavigationFrame.h"
 
