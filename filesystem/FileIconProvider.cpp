1bc643098c18db5724112d35b2c03f3108ee2c01
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2013-05-20 11:24:56 +0000

    moved icon effects upstream to avoid duplicated code

--- filesystem/FileIconProvider.cpp
+++ filesystem/FileIconProvider.cpp
@@ -57,63 +57,23 @@ const QIcon& FileIconProvider::icon( const FileRecord& fileRecord )
     if( iter != icons_.end() ) return iter.value();
 
     // create
-    QIcon base;
-    if( type & FileSystemModel::Navigator ) base = IconEngine::get( ICONS::PARENT );
-    else if( type & FileSystemModel::Folder ) base = IconEngine::get( ICONS::FOLDER );
+    QIcon out;
+    if( type & FileSystemModel::Navigator ) out = IconEngine::get( ICONS::PARENT );
+    else if( type & FileSystemModel::Folder ) out = IconEngine::get( ICONS::FOLDER );
     else if( type & FileSystemModel::Document )
     {
 
         // try get icon from mimetypes
-        base = mimeTypeIconProvider_.icon( fileRecord.file().extension() );
-        if( base.isNull() ) base = IconEngine::get( ICONS::DOCUMENT );
+        out = mimeTypeIconProvider_.icon( fileRecord.file().extension() );
+        if( out.isNull() ) out = IconEngine::get( ICONS::DOCUMENT );
 
     }
 
-    {
-        // normal icon. Only copy relevant pixmaps
-        QIcon copy( base );
-        base = QIcon();
-        QIcon linkOverlay( IconEngine::get( ICONS::LINK ) );
-        foreach( const QSize& size, copy.availableSizes() )
-        { base.addPixmap( copy.pixmap( size ) ); }
-
-    }
-
-    // add link overlay
-    if( type & FileSystemModel::Link )
-    {
-
-        QIcon copy( base );
-        base = QIcon();
-        QIcon linkOverlay( IconEngine::get( ICONS::LINK ) );
-        foreach( const QSize& size, copy.availableSizes() )
-        {
-
-            QSize overlaySize;
-            if( size.width() <= 16 ) overlaySize = QSize( 8, 8 );
-            else if( size.width() <= 22 ) overlaySize = QSize( 8, 8 );
-            else if( size.width() <= 32 ) overlaySize = QSize( 12, 12 );
-            else if( size.width() <= 48 ) overlaySize = QSize( 16, 16 );
-            else if( size.width() <= 64 ) overlaySize = QSize( 22, 22 );
-            else if( size.width() <= 128 ) overlaySize = QSize( 48, 48 );
-            else overlaySize = QSize( 64, 64 );
-
-            base.addPixmap( CustomPixmap( copy.pixmap( size ) ).merge( linkOverlay.pixmap( overlaySize ), CustomPixmap::BOTTOM_RIGHT ) );
-
-        }
-    }
-
-    // hidden file
-    if( type & FileSystemModel::Hidden )
-    {
-        QIcon copy( base );
-        base = QIcon();
-        foreach( const QSize& size, copy.availableSizes() )
-        { base.addPixmap( CustomPixmap( copy.pixmap( size ) ).transparent( 0.6 ) ); }
-
-    }
+    out = _copy( out );
+    if( type & FileSystemModel::Link ) out = _linked( out );
+    if( type & FileSystemModel::Hidden ) out = _hidden( out );
 
     // insert in map and return
-    return icons_.insert( key, base ).value();
+    return icons_.insert( key, out ).value();
 
 }
